e3c9d11e367a02822a0e3ce317a33f33bdc74f35
J_sky <bosichong@qq.com>
2017-11-20 21:32:01 +0800

    实战：利用Django开发部署自己的个人博客（4）Models 数据模型

--- Django/blog01/myblog/models.py
+++ Django/blog01/myblog/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-# Create your models here.
+#创建文章表，通过继承models.Model来创建文章表
+class Article(models.Model):
+    title = models.CharField(max_length=50, verbose_name = 'blog标题', default = '')#创建标题
+    content = models.TextField(verbose_name = '博客正文', default = '')

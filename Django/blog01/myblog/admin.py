cf36578c6d1ccd2e90ee2a11b9232f86887513f8
J_sky <bosichong@qq.com>
2017-11-23 18:52:25 +0800

    实战：利用Django开发部署自己的个人博客（5）Django admin后台管理

--- Django/blog01/myblog/admin.py
+++ Django/blog01/myblog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Article, Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    #设置显示
+    list_display = ('id','title',)
+
+class ArticleAadmin(admin.ModelAdmin):
+    list_display = ('id','title','content',)
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Article, ArticleAadmin)
--- Django/blog01/myblog/models.py
+++ Django/blog01/myblog/models.py
@@ -1,5 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User#引入Django User
+
+
+class Category(models.Model):
+    #分类表
+    title = models.CharField(max_length=20, verbose_name = '分类名称', default = '')
+
+    class Meta:
+        verbose_name = u'博客分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+
 #创建文章表，通过继承models.Model来创建文章表
 class Article(models.Model):
-    title = models.CharField(max_length=50, verbose_name = 'blog标题', default = '')#创建标题
+    #文章表
+    title = models.CharField(max_length=50, verbose_name = '文章标题', default = '')#创建标题
     content = models.TextField(verbose_name = '博客正文', default = '')
+    category = models.ForeignKey(Category, verbose_name = '所属分类', default = '')
+    user = models.ForeignKey(User, verbose_name='文章作者', null=True, blank=True)
+    #文章创建时间，这里的auto_now_add=True表示自动在数据库里创建时间。
+    create_time = models.DateTimeField(verbose_name='文章创建时间', auto_now_add=True,)
+
+    class Meta:
+        verbose_name = u'博客文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+
+
+
+    
+    
